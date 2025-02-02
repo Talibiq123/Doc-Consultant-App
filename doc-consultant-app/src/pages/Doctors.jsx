fa2d00c6726819f7ad01b4aac68301cc8e1e5b82
Talibiq123 <talibsaeed90@gmail.com>
2025-02-02 21:22:48 +0530

    Doctors components

--- doc-consultant-app/src/pages/Doctors.jsx
+++ doc-consultant-app/src/pages/Doctors.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Doctors = () => {
+
+  const { speciality } = useParams();
+
+  const { doctors } = useContext(AppContext);
+  
+
   return (
     <div>
-      
+      <p>Browse through the doctors speciality.</p>
+      <div>
+        <div>
+          <p>General Physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+      </div>
     </div>
   )
 }
