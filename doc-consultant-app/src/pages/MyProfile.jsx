a73897ccebcb1c018a485920c3fbf96b886fd668
Talibiq123 <talibsaeed90@gmail.com>
2025-02-07 21:57:47 +0530

    profile page is creating

--- doc-consultant-app/src/pages/MyProfile.jsx
+++ doc-consultant-app/src/pages/MyProfile.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: "Edward Vincent",
+    image: assets.profile_pic,
+    email: 'rechardcindnt@gmail.com',
+    phone: '+1 123 456 7890',
+    address: {
+      line1: "57th Cross Redmond",
+      line2: "Circle, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '2000-01-20'
+  });
+
+  const [isEdit, setIsEdit] = useState(false);
+
   return (
     <div>
-      
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit ?
+        <input type="text" value={userData.name} onChange={ e => setUserData(prev => ({...prev, name: e.target.value}))} /> :
+        <p>{userData.name}</p>
+      }
     </div>
   )
 }
 
-export default MyProfile
+export default MyProfile;
