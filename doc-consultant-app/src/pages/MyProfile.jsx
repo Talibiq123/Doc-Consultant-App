892e61476219493ee4d6336dcec75d566476ab95
Talibiq123 <talibsaeed90@gmail.com>
2025-02-11 22:20:55 +0530

    MyProfile component completed.

--- doc-consultant-app/src/pages/MyProfile.jsx
+++ doc-consultant-app/src/pages/MyProfile.jsx
@@ -34,15 +34,16 @@ const MyProfile = () => {
         <p className=" font-medium text-3xl text-neutral-800 mt-4">{userData.name}</p>
       )}
 
-      <hr />
+      <hr className=" bg-zinc-400 h-[1px] border-none" />
       <div>
-        <p>CONTACT INFORMATION</p>
-        <div>
-          <p>Email Id:</p>
-          <p>{userData.email}</p>
-          <p>Phone:</p>
+        <p className=" text-neutral-500 underline mt-3">CONTACT INFORMATION</p>
+        <div className=" grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+          <p className=" font-medium">Email Id:</p>
+          <p className=" text-blue-500">{userData.email}</p>
+          <p className=" font-medium">Phone:</p>
           {isEdit ? (
             <input
+              className=" bg-gray-100 max-w-52"
               type="text"
               value={userData.phone}
               onChange={(e) =>
@@ -50,12 +51,13 @@ const MyProfile = () => {
               }
             />
           ) : (
-            <p>{userData.phone}</p>
+            <p className=" text-blue-400">{userData.phone}</p>
           )}
-          <p>Address:</p>
+          <p className=" font-medium">Address:</p>
           {isEdit ? (
             <p>
               <input
+                className=" bg-gray-100"
                 onChange={(e) =>
                   setUserData((prev) => ({
                     ...prev,
@@ -67,6 +69,7 @@ const MyProfile = () => {
               />
               <br />
               <input
+                className=" bg-gray-100"
                 onChange={(e) =>
                   setUserData((prev) => ({
                     ...prev,
@@ -78,7 +81,7 @@ const MyProfile = () => {
               />
             </p>
           ) : (
-            <p>
+            <p className=" text-gray-500">
               {userData.address.line1}
               <br />
               {userData.address.line2}
@@ -88,11 +91,12 @@ const MyProfile = () => {
       </div>
 
       <div>
-        <p>BASIC INFORMATION</p>
-        <div>
-          <p>Gender:</p>
+        <p className=" text-neutral-500 underline mt-3">BASIC INFORMATION</p>
+        <div className=" grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700">
+          <p className=" font-medium">Gender:</p>
           {isEdit ? (
             <select
+              className=" max-w-20 bg-gray-100"
               onChange={(e) =>
                 setUserData((prev) => ({ ...prev, gender: e.target.value }))
               }
@@ -102,13 +106,13 @@ const MyProfile = () => {
               <option value="Female">Female</option>
             </select>
           ) : (
-            <p>{userData.gender}</p>
+            <p className=" text-gray-400">{userData.gender}</p>
           )}
-          <p>Birthday:</p>
+          <p className=" font-medium">Birthday:</p>
           {
             isEdit
-              ? <input type="date" onChange={(e) => setUserData((prev) => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
-              : <p>{userData.dob}</p>
+              ? <input className=" max-w-28 bg-gray-100" type="date" onChange={(e) => setUserData((prev) => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
+              : <p className=" text-gray-400">{userData.dob}</p>
           }
         </div>
       </div>
@@ -116,8 +120,8 @@ const MyProfile = () => {
       <div>
         {
           isEdit
-            ? <button onClick={() => setIsEdit(false)}>Save Information</button>
-            : <button onClick={() => setIsEdit(true)}>Edit</button>
+            ? <button className=" border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all" onClick={() => setIsEdit(false)}>Save Information</button>
+            : <button className=" border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all" onClick={() => setIsEdit(true)}>Edit</button>
         }
       </div>
 
